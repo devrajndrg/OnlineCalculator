d5935a44211082f7c62a219b0b3dc94fc380060f
Devraj <devaraja.r@ellucian.com>
2017-12-18 11:01:56 +0530

    Initial commit

---
+++ src/main/java/com/demo/example/OnlineCalculator.java
@@ -0,0 +1,9 @@
+package com.demo.example;
+
+public class OnlineCalculator {
+	
+	public int add (int a, int b){
+		return a+b;
+	}
+
+}
---
+++ src/test/java/com/demo/example/OnlineCalculatortest.java
@@ -0,0 +1,23 @@
+package com.demo.example;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class OnlineCalculatortest {
+
+	static OnlineCalculator obj;
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		obj= new OnlineCalculator();
+		
+	}
+
+	@Test
+	public void testAdd() {
+		int result = obj.add(2, 3);
+		assertEquals(5,result);
+	}
+
+}
