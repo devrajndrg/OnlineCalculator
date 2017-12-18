8f3bd71092fb66853d2112ef4bcf39b6ef487148
Devraj <devaraja.r@ellucian.com>
2017-12-18 13:18:22 +0530

    3rd commit

--- src/test/java/com/demo/example/OnlineCalTest.java
+++ src/test/java/com/demo/example/OnlineCalTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.*;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class OnlineCalculatorTest {
+public class OnlineCalTest {
 
 	static OnlineCalculator obj;
 	@BeforeClass
